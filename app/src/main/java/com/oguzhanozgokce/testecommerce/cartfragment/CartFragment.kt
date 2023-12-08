066fd41d97fc1eacab9a7e1273f223424348345a
Oğuzhan ÖZGÖKCE <ozgokceoguzhan34@gmail.com>
2023-12-08 14:34:27 +0300

    update commit

--- app/src/main/java/com/oguzhanozgokce/testecommerce/cartfragment/CartFragment.kt
+++ app/src/main/java/com/oguzhanozgokce/testecommerce/cartfragment/CartFragment.kt
@@ -1,21 +1,21 @@
-package com.oguzhanozgokce.testecommerce
+package com.oguzhanozgokce.testecommerce.cartfragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.oguzhanozgokce.testecommerce.R
 
-class FavoritesFragment : Fragment() {
 
-
+class CartFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_favorites, container, false)
+        return inflater.inflate(R.layout.fragment_cart, container, false)
     }
 
 
--- app/src/main/java/com/oguzhanozgokce/testecommerce/homefragment/HomeFragment.kt
+++ app/src/main/java/com/oguzhanozgokce/testecommerce/homefragment/HomeFragment.kt
@@ -1,10 +1,11 @@
-package com.oguzhanozgokce.testecommerce
+package com.oguzhanozgokce.testecommerce.homefragment
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.oguzhanozgokce.testecommerce.R
 
 
 class HomeFragment : Fragment() {
